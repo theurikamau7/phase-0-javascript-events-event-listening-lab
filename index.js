6c4151cf0092198ae5de41c7ff3f5bc6e5f4c46c
IanKamau <theurikamau7@gmail.com>
2023-07-27 00:51:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
+const btn = document.getElementById("button")
+
+function clickAlert() {
+    alert('I was clicked!');
 }
+
+function addingEventListener() {
+    alert('I was clicked!');
+    btn.addEventListener("click", clickAlert);
+}
+
+addingEventListener();
